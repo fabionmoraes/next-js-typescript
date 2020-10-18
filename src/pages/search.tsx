a29f566a9cb6c2d356a31de17f73397455a0e3f4
fabionmoraes <fabio@elaboredigital.com.br>
2020-10-18 20:29:54 -0300

    update 2020-10-18

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from "next/router";
+import { FormEvent, useCallback, useState } from "react"
+
 export default function Search() {
+    const router = useRouter();
+    const [search, setSearch] = useState('');
+
+    const handleSearch = useCallback((e: FormEvent) => {
+        e.preventDefault();
+
+        router.push(`/search?q=${encodeURIComponent(search)}`)
+
+        setSearch('');
+    }, [router, search]);
+
     return (
         <div>
-            <h2>Search</h2>
+            <form onSubmit={handleSearch}>
+                <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+                <button type="submit">Search</button>
+            </form>
         </div>
     )
 }
